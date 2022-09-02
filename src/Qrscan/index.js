0d64a286f9801edc70f07ab53c76879330080fce
Olof Hultberg <olofhultberg@Olofs-MacBook-Pro.local>
2022-09-02 13:21:37 +0200

    First commit

---
+++ src/Qrscan/index.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import {QrReader}  from "react-qr-reader";
+
+const Qrscan =()=>{
+    const [result, setResult] = useState('No result');
+    
+    const handleError = (err) =>{
+        console.err(err)
+    }
+
+    const handleScan = (result) =>{
+        if(result){
+            setResult(result);
+        }
+    }
+    
+    const previewStyle = {
+        height: 240,
+        width: 320,
+    }
+
+    return (
+        <div className="styles.container">
+           
+            <QrReader 
+                delay={500}
+                style={previewStyle}
+                onError={handleError}
+                onScan={handleScan}
+            />
+            <div className="styles.result">{result}</div>
+        </div>
+    )
+}
+
+export default Qrscan;
